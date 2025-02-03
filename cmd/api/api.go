f0a1758ddb90173f9072474ccbc4da6088d0a149
Omprakash  D <omugan07@gmail.com>
2025-02-03 15:20:14 +0530

    chore: air config for hot reload

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -1,2 +1,26 @@
 package api
 
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"gorm.io/gorm"
+)
+
+type APIServer struct {
+	addr string
+	db   *gorm.DB
+}
+
+func NewAPIServer(addr string, db *gorm.DB) *APIServer {
+	return &APIServer{addr: addr, db: db}
+}
+
+func (s *APIServer) Run() error {
+	app := fiber.New()
+	api := app.Group("/api/v1")
+
+	log.Printf("Server is running on %s", s.addr)
+
+	return app.Listen(s.addr)
+}
--- cmd/main.go
+++ cmd/main.go
@@ -1,3 +1,15 @@
 package main
 
-func main() {}
+import (
+	"log"
+
+	"github.com/OmprakashD20/go-todo-api/cmd/api"
+)
+
+func main() {
+	server := api.NewAPIServer(":8000", nil)
+
+	if err := server.Run(); err != nil {
+		log.Fatal("Failed to run the server")
+	}
+}
