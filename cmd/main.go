c13e28693b41eb91e7ea393e9fd210df7ae38f20
Omprakash  D <omugan07@gmail.com>
2025-02-03 16:48:27 +0530

    chore: setup database connection

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,18 @@ import (
 	"log"
 
 	"github.com/OmprakashD20/go-todo-api/cmd/api"
+	"github.com/OmprakashD20/go-todo-api/config"
+	"github.com/OmprakashD20/go-todo-api/database"
 )
 
 func main() {
-	server := api.NewAPIServer(":8000", nil)
+	_, err := database.ConnectDB(&config.Envs.DB)
+
+	if err != nil {
+		log.Fatalf("Failed to connect to the database")
+	}
+
+	server := api.NewAPIServer(config.Envs.Port, nil)
 
 	if err := server.Run(); err != nil {
 		log.Fatal("Failed to run the server")
--- database/database.go
+++ database/database.go
@@ -1,1 +1,25 @@
 package database
+
+import (
+	"fmt"
+
+	"github.com/OmprakashD20/go-todo-api/config"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func ConnectDB(config *config.DBConfig) (*gorm.DB, error) {
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		config.DBHost, config.DBPort, config.DBUser, config.DBPort, config.DBName, config.SSLMode,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
