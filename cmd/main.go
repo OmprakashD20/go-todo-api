a4d151047a06868ee857aeeea86076a0ad8620c2
Omprakash  D <omugan07@gmail.com>
2025-02-03 20:37:13 +0530

    chore: setup database migrations

--- cmd/main.go
+++ cmd/main.go
@@ -4,18 +4,23 @@ import (
 	"log"
 
 	"github.com/OmprakashD20/go-todo-api/cmd/api"
+	"github.com/OmprakashD20/go-todo-api/cmd/migrate"
 	"github.com/OmprakashD20/go-todo-api/config"
 	"github.com/OmprakashD20/go-todo-api/database"
 )
 
 func main() {
-	_, err := database.ConnectDB(&config.Envs.DB)
+	db, err := database.ConnectDB(&config.Envs.DB)
 
 	if err != nil {
 		log.Fatalf("Failed to connect to the database")
 	}
 
-	server := api.NewAPIServer(config.Envs.Port, nil)
+	if err := migrate.Migrate(db); err != nil {
+		log.Fatalf("Failed to migrate the database")
+	}
+
+	server := api.NewAPIServer(config.Envs.Port, db)
 
 	if err := server.Run(); err != nil {
 		log.Fatal("Failed to run the server")
